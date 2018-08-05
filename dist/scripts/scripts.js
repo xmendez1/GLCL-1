d49e3eb46d7dbfdfe91c81727468b14a63b5f8c8
Kathy Zhu <kzhu@c02vv37ghv2n.attlocal.net>
2018-08-05 11:52:13 -0700

    completed first iteration of buttons

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,6 +1,17 @@
-"use strict";
+'use strict';
 
-var button = {};
+var fn = function fn() {
+	var minus = document.getElementsByClassName('minus');
+	console.log(minus);
+	for (var i = 0; i < minus.length; i++) {
+		console.log(minus[i]);
+		var e = document.createElement('p');
+		e.innerHTML = '-';
+		minus[i].appendChild(e);
+	}
+};
+
+document.addEventListener('DOMContentLoaded', fn, false);
 'use strict';
 
 var list = [];
