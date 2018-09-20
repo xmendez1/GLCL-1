1ddd148d20d15381b8fab59afb51f6381a8a793c
Matthew Johnson <mattmjohnson33@gmail.com>
2018-09-20 14:47:06 -0700

    added development instructions to project

--- assets/calendar/calendar.js
+++ assets/calendar/calendar.js
@@ -1,17 +1,50 @@
 let calendar = () => {
 
-  let firstDay = (year, month) => {
+  const firstDay = (year, month) => {
     return new Date(year, month - 1, 1).getDay();
   }
 
-  let lastDay = (year, month) => {
+  const lastDay = (year, month) => {
     return new Date(year, month, 0).getDate();
   }
 
-  let daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  //TODO: Create currentTime, currentDay, currentMonth, currentYear variables based on this stack overflow answer
+  //https://stackoverflow.com/a/6002265/5885911
+  //Use const rather than let or var, we use const when a variable doesn't get reassigned or updated overtime.
+  //Since these are functions, we don't expect the functions to change so we can make them constants.
 
-  var calendarHead = document.getElementById('calendar-head');
-  let populateCalendarHead = () => {
+
+  //TODO: Create the lastMonth function which takes the arguments, "year", "month", "dayOfWeek"
+  //Create the previousMonth variable which is equal to month-1
+  //Create the daysInPreviousMonth function which takes two arguments, "y", "p".
+  // Y represents current year, p represents previousMonth
+    //Within function
+    //Create a variable called previousMonthArr which is equal to an emptyArr,
+    //you would use let for this variable since we will be pushing items to the array
+    //Create an ld variable which is equal to the lastDay function and takes the arguments, y and p
+    //Create a variable i which is equal to 0, we will use let here since i is an index and will be updated
+    //Create a while loop, which says while the index is less than daysOfTheWeek
+      //Inside while loop
+      //push ld to the previousMonthArr
+      //decrement ld
+      //increment i
+    //outside of while loop, return previousMonthArr
+  //outside of daysInPreviousMonth function
+  //if previous month is less than 1
+    //previous month = 12 (we're setting the previous month to December, if the current month is January)
+    //return the daysInPreviousMonth function which takes the arguments year-1 and previousMonth
+  //else
+    //return the daysInPreviousMonth function which takes the arguments year and previousMonth
+//end lastMonth function
+
+
+
+
+
+  const daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  const calendarHead = document.getElementById('calendar-head');
+  const populateCalendarHead = () => {
     for(let i = 0; i<daysOfTheWeek.length; i++){
       let day = document.createElement('p');
       day.innerHTML = daysOfTheWeek[i];
@@ -20,25 +53,40 @@ let calendar = () => {
   }
   populateCalendarHead();
 
-  let calendarBody = document.getElementById('calendar-body');
-  let populateCalendarBody = () => {
-    let daysInRow = 7;
-    let totalRows = 6;
-    // var totalDays = daysInRow * totalRows;
+  const calendarBody = document.getElementById('calendar-body');
+  const populateCalendarBody = () => {
+    const currentDaysInMonth = lastDay(2018, 9);
+    const firstDayOfMonth = firstDay(2018, 9);
+    //TODO: Create lastMonthArr const which is equal to the lastMonth function which
+    // takes the arguments currentYear, currentMonth, and firstDayOfMonth
+    const daysInRow = 7;
+    const totalRows = 6;
     let currentRows = 0;
-    let currentDaysInMonth = lastDay(2018, 9);
-    let firstDayOfMonth = firstDay(2018, 9);
-    let currentDayOfMonthIndex = 1;
-    let currentCalendarDayIndex = 0;
+    let currentDayOfMonthIndex = 1; //current day of current month (e.g. september)
+    let currentCalendarDayIndex = 0; //current index of days on calendar (i.e. daysInRow * totalRows)
+    //TODO: Create lmaIndex variable which is equal to the firstDayOfMonth-1
 
     while(currentRows < totalRows){
-      let calendarRow = document.createElement('div');
+      const calendarRow = document.createElement('div');
       for(let i = 0; i<daysInRow; i++){
-        let day = document.createElement('p');
+        const day = document.createElement('p');
+        //TODO: Create a new if condition which says
+        //"If the currentCalendarDayIndex is less than the firstDayOfMonth"
+          //Within the if condition
+          //set day inner html to the current lmaIndex of the lastMonthArr
+          //add the 'grayedOut' class to the classList of the "day" variable
+          //decrement the lmaIndex
+
+        //This if condition says "start adding days to the calendar, if the
+        //first weekday of the current month has been reached"
         if(currentCalendarDayIndex>=firstDayOfMonth && currentDayOfMonthIndex <= currentDaysInMonth){
           day.innerHTML = currentDayOfMonthIndex;
           currentDayOfMonthIndex++;
         }
+
+        //TODO: BONUS: You can add another if condition here if you want, which will just say "add grayed numbers to the end of the month until we run out of space on the calendar"
+
+
         currentCalendarDayIndex++;
         calendarRow.appendChild(day);
       }
--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,5 +1,104 @@
 'use strict';
 
+var calendar = function calendar() {
+
+  var firstDay = function firstDay(year, month) {
+    return new Date(year, month - 1, 1).getDay();
+  };
+
+  var lastDay = function lastDay(year, month) {
+    return new Date(year, month, 0).getDate();
+  };
+
+  //TODO: Create currentTime, currentDay, currentMonth, currentYear variables based on this stack overflow answer
+  //https://stackoverflow.com/a/6002265/5885911
+  //Use const rather than let or var, we use const when a variable doesn't get reassigned or updated overtime.
+  //Since these are functions, we don't expect the functions to change so we can make them constants.
+
+
+  //TODO: Create the lastMonth function which takes the arguments, "year", "month", "dayOfWeek"
+  //Create the previousMonth variable which is equal to month-1
+  //Create the daysInPreviousMonth function which takes two arguments, "y", "p".
+  // Y represents current year, p represents previousMonth
+  //Within function
+  //Create a variable called previousMonthArr which is equal to an emptyArr,
+  //you would use let for this variable since we will be pushing items to the array
+  //Create an ld variable which is equal to the lastDay function and takes the arguments, y and p
+  //Create a variable i which is equal to 0, we will use let here since i is an index and will be updated
+  //Create a while loop, which says while the index is less than daysOfTheWeek
+  //Inside while loop
+  //push ld to the previousMonthArr
+  //decrement ld
+  //increment i
+  //outside of while loop, return previousMonthArr
+  //outside of daysInPreviousMonth function
+  //if previous month is less than 1
+  //previous month = 12 (we're setting the previous month to December, if the current month is January)
+  //return the daysInPreviousMonth function which takes the arguments year-1 and previousMonth
+  //else
+  //return the daysInPreviousMonth function which takes the arguments year and previousMonth
+  //end lastMonth function
+
+
+  var daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  var calendarHead = document.getElementById('calendar-head');
+  var populateCalendarHead = function populateCalendarHead() {
+    for (var i = 0; i < daysOfTheWeek.length; i++) {
+      var day = document.createElement('p');
+      day.innerHTML = daysOfTheWeek[i];
+      calendarHead.appendChild(day);
+    }
+  };
+  populateCalendarHead();
+
+  var calendarBody = document.getElementById('calendar-body');
+  var populateCalendarBody = function populateCalendarBody() {
+    var currentDaysInMonth = lastDay(2018, 9);
+    var firstDayOfMonth = firstDay(2018, 9);
+    //TODO: Create lastMonthArr const which is equal to the lastMonth function which
+    // takes the arguments currentYear, currentMonth, and firstDayOfMonth
+    var daysInRow = 7;
+    var totalRows = 6;
+    var currentRows = 0;
+    var currentDayOfMonthIndex = 1; //current day of current month (e.g. september)
+    var currentCalendarDayIndex = 0; //current index of days on calendar (i.e. daysInRow * totalRows)
+    //TODO: Create lmaIndex variable which is equal to the firstDayOfMonth-1
+
+    while (currentRows < totalRows) {
+      var calendarRow = document.createElement('div');
+      for (var i = 0; i < daysInRow; i++) {
+        var day = document.createElement('p');
+        //TODO: Create a new if condition which says
+        //"If the currentCalendarDayIndex is less than the firstDayOfMonth"
+        //Within the if condition
+        //set day inner html to the current lmaIndex of the lastMonthArr
+        //add the 'grayedOut' class to the classList of the "day" variable
+        //decrement the lmaIndex
+
+        //This if condition says "start adding days to the calendar, if the
+        //first weekday of the current month has been reached"
+        if (currentCalendarDayIndex >= firstDayOfMonth && currentDayOfMonthIndex <= currentDaysInMonth) {
+          day.innerHTML = currentDayOfMonthIndex;
+          currentDayOfMonthIndex++;
+        }
+
+        //TODO: BONUS: You can add another if condition here if you want, which will just say "add grayed numbers to the end of the month until we run out of space on the calendar"
+
+
+        currentCalendarDayIndex++;
+        calendarRow.appendChild(day);
+      }
+      calendarBody.appendChild(calendarRow).className = "calendar-row";
+      currentRows++;
+    }
+  };
+  populateCalendarBody();
+};
+
+document.addEventListener('DOMContentLoaded', calendar, false);
+'use strict';
+
 var fn = function fn() {
 	// adding p tag for minus
 	var minus = document.getElementsByClassName('minus');
@@ -22,58 +121,10 @@ var fn = function fn() {
 document.addEventListener('DOMContentLoaded', fn, false);
 'use strict';
 
-var calendar = function calendar() {
-
-  var firstDay = function firstDay(year, month) {
-    return new Date(year, month - 1, 1).getDay();
-  };
-
-  var lastDay = function lastDay(year, month) {
-    return new Date(year, month, 0).getDate();
-  };
-
-  var daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-
-  var calendarHead = document.getElementById('calendar-head');
-  var populateCalendarHead = function populateCalendarHead() {
-    for (var i = 0; i < daysOfTheWeek.length; i++) {
-      var day = document.createElement('p');
-      day.innerHTML = daysOfTheWeek[i];
-      calendarHead.appendChild(day);
-    }
-  };
-  populateCalendarHead();
-
-  var calendarBody = document.getElementById('calendar-body');
-  var populateCalendarBody = function populateCalendarBody() {
-    var daysInRow = 7;
-    var totalRows = 6;
-    // var totalDays = daysInRow * totalRows;
-    var currentRows = 0;
-    var currentDaysInMonth = lastDay(2018, 9);
-    var firstDayOfMonth = firstDay(2018, 9);
-    var currentDayOfMonthIndex = 1;
-    var currentCalendarDayIndex = 0;
-
-    while (currentRows < totalRows) {
-      var calendarRow = document.createElement('div');
-      for (var i = 0; i < daysInRow; i++) {
-        var day = document.createElement('p');
-        if (currentCalendarDayIndex >= firstDayOfMonth && currentDayOfMonthIndex <= currentDaysInMonth) {
-          day.innerHTML = currentDayOfMonthIndex;
-          currentDayOfMonthIndex++;
-        }
-        currentCalendarDayIndex++;
-        calendarRow.appendChild(day);
-      }
-      calendarBody.appendChild(calendarRow).className = "calendar-row";
-      currentRows++;
-    }
-  };
-  populateCalendarBody();
+var list = [];
+var myFunkyFunk = function myFunkyFunk() {
+  console.log('the funk');
 };
-
-document.addEventListener('DOMContentLoaded', calendar, false);
 'use strict';
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
@@ -123,9 +174,3 @@ var titleCarouselWrapper = function titleCarouselWrapper() {
 };
 
 document.addEventListener('DOMContentLoaded', titleCarouselWrapper, false);
-'use strict';
-
-var list = [];
-var myFunkyFunk = function myFunkyFunk() {
-  console.log('the funk');
-};
